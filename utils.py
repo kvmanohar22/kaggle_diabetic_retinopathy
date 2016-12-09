2efeff0ed5cec1022add199ec637b875978e0f11
kvmanohar22 <kvmanohar22@gmail.com>
2016-12-10 00:10:47 +0530

    update

--- utils.py
+++ utils.py
@@ -71,11 +71,11 @@ def split_data(train_labels, labels_split, valid_size=20,
 
             sss = StratifiedShuffleSplit(label_pairs.values, n_iter=1,
                                          test_size=0.01 * valid_size,
-                                         indices=None, random_state=SEED)
+                                         random_state=SEED)
         else:
             sss = StratifiedShuffleSplit(train_labels.level, n_iter=1,
                                          test_size=0.01 * valid_size,
-                                         indices=None, random_state=SEED)
+                                         random_state=SEED)
         for ix_train, ix_test in sss:
             pass
             # TODO: has no next(), need to figure this out
